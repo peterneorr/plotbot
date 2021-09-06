72c9e40eefb29aa210214995a6b03dc4d708f882
Peter Neorr <peterneorr@gmail>
2021-09-06 07:39:50 +0100

    adds some demos

---
+++ plotxy.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+import logging
+import threading
+import sys
+import time
+import threading
+
+import RPi.GPIO as GPIO
+from pb.home_sensor import HomeSensor
+from pb.homing_motor import HomingMotor
+from pb.stepper import Stepper
+
+
+def init_x() -> HomingMotor:
+    dir = 5
+    step = 6
+    ms1 = 26
+    ms2 = 19
+    ms3 = 13
+    sensor = HomeSensor(24)
+    stepper = Stepper(dir_pin=dir, step_pin=step, ms1_pin=ms1, ms2_pin=ms2, ms3_pin=ms3)
+    xmax = 904
+    m = HomingMotor("X-Motor", stepper, sensor, xmax, False)
+    return m
+
+
+def init_y() -> HomingMotor:
+    y_dir = 27
+    y_step = 22
+    y_ms1 = 9
+    y_ms2 = 10
+    y_ms3 = 11
+    stepper = Stepper(dir_pin=y_dir, step_pin=y_step, ms1_pin=y_ms1, ms2_pin=y_ms2, ms3_pin=y_ms3)
+    sensor = HomeSensor(23)
+    ymax = 950
+    m = HomingMotor("Y-Motor", stepper, sensor, ymax, False, .001)
+    m.set_step_size(2)
+    return m
+
+
+def init_z() -> HomingMotor:
+    z_dir = 1
+    z_step = 12
+    z_ms1 = 21
+    z_ms2 = 20
+    z_ms3 = 16
+    stepper = Stepper(dir_pin=z_dir, step_pin=z_step, ms1_pin=z_ms1, ms2_pin=z_ms2, ms3_pin=z_ms3)
+    sensor = HomeSensor(25)
+    zmax = 3422
+    m = HomingMotor("Z-Motor", stepper, sensor, zmax, True)
+    return m
+
+
+
+
+def go_percent(m: HomingMotor, percent: float):
+    pos = percent * m.get_max_steps()
+    m.goto_pos(pos)
+
+
+def main():
+    GPIO.setmode(GPIO.BCM)
+    x, y, z = init_x(), init_y(), init_z()
+    pen_down = lambda: z.goto_pos(2000)
+    pen_up = lambda: z.goto_pos(1450)
+
+    def border():
+        # draw bounding box
+        x.go_home()
+        y.go_home()
+        pen_down();
+        x.goto_pos(x.get_max_steps())
+        y.goto_pos(y.get_max_steps())
+        x.go_home()
+        y.go_home()
+        pen_up()
+
+    time.sleep(1)
+    try:
+
+        x.go_home()
+        y.go_home()
+        z.go_home()
+
+        for j in range(1, 100):
+            go_percent(y, j / 100)
+            for i in range(1, 100):
+                go_percent(x, i / 100)
+                pen_down()
+                pen_up()
+        z.go_home()
+        GPIO.cleanup()
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    main()
